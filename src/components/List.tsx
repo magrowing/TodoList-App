6ba694a08c321e3fca842b1474dd892429f9d828
magrowing <kmagrowing@gmail.com>
2024-04-17 23:46:56 +0900

    refactor : 할일 목록 조건에 따라 출력 문구 변경 첫 페이지 로드 / 조회된 할일 목록 리스트 두가지 경우 반영하여 출력 문구 수정

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import styled from 'styled-components';
 
 import ListItem from './ListItem';
@@ -9,15 +11,41 @@ const LisContainer = styled.ul`
   border-radius: 1.2rem;
   margin: 2rem 0;
   background-color: ${(props) => props.theme.colors.bgSecondary};
+
+  &.none-msg {
+    background-color: transparent;
+    li {
+      color: ${(props) => props.theme.colors.primary};
+      font-weight: 600;
+      text-align: center;
+    }
+  }
 `;
 
 function List() {
   const todoItems = useTodoStore((state) => state.todoItems);
+  const [localStorageTodos, setLocalStorageTodos] = useState([]);
+
+  useEffect(() => {
+    const localTodoItems = localStorage.getItem('todoItems');
+    if (localTodoItems) {
+      setLocalStorageTodos(JSON.parse(localTodoItems));
+    }
+    setLocalStorageTodos([]);
+  }, [todoItems]);
+
+  if (!localStorageTodos.length) {
+    return (
+      <LisContainer className="none-msg">
+        <li>할일을 추가해주세요!</li>
+      </LisContainer>
+    );
+  }
 
   if (!todoItems.length) {
     return (
-      <LisContainer>
-        <li>할일을 추가해주세요.</li>
+      <LisContainer className="none-msg">
+        <li>할일 목록이 존재하지 않습니다.</li>
       </LisContainer>
     );
   }
