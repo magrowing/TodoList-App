a2c979a65972128d99b23027f33e7798d0e4e137
magrowing <kmagrowing@gmail.com>
2024-04-22 18:26:19 +0900

    refactor : Build Error DateTimeFormat Options Type Error

--- src/utils/dateTimeFormat.ts
+++ src/utils/dateTimeFormat.ts
@@ -1,15 +1,12 @@
-
-const options = {
-  weekday: 'long',
-  year: 'numeric', // 연도를 숫자로 표시
-  month: 'long',
-  day: 'numeric', // 날짜를 숫자로 표시
-  hour: 'numeric', // 시간을 숫자로 표시
-  minute: 'numeric', // 분을 숫자로 표시
-};
-
 function dateTimeFormat(data : number ) {
-  return (new Intl.DateTimeFormat('ko-KR', options).format(data));
+  return (new Intl.DateTimeFormat('ko-KR', {
+    weekday: 'long',
+    year: 'numeric', 
+    month: 'long',
+    day: 'numeric', 
+    hour: 'numeric', 
+    minute: 'numeric',
+  }).format(data));
 }
 
 export default dateTimeFormat;
